892ff0d677eaa9867a8d87a14497767e59a0db31
F <f@replicate.com>
2023-01-25 14:17:32 +0000

    Remove stray logging

--- pages/api/predictions/[id].js
+++ pages/api/predictions/[id].js
@@ -1,7 +1,5 @@
 const API_HOST = process.env.REPLICATE_API_HOST || "https://api.replicate.com";
 
-console.log({ API_HOST });
-
 export default async function handler(req, res) {
   const response = await fetch(`${API_HOST}/v1/predictions/${req.query.id}`, {
     headers: {

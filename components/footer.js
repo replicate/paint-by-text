6758e8bbf0689998eb1aeb197d4eda15425f7bec
Zeke Sikelianos <zeke@sikelianos.com>
2023-01-30 11:37:17 -0800

    are you a developer?

--- components/footer.js
+++ components/footer.js
@@ -43,6 +43,16 @@ export default function Footer({ events, startOver, handleImageDropped }) {
           </a>
         </Link>
       </div>
+
+      <div className="text-center lil-text mt-8">
+      <div className="inline-block py-2 px-4 border border-yellow-200 rounded-lg bg-[#fef6aa]">
+      🤔 Are you a developer and want to learn how to build this? Check out the{" "}
+        <Link href="https://github.com/replicate/paint-with-words#readme">
+          <a target="_blank">README</a>
+        </Link>.
+      </div>
+      </div>
+
       <div className="text-center lil-text mt-8">
         Powered by{" "}
         <Link href="https://www.timothybrooks.com/instruct-pix2pix/">

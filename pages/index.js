9c9289d57e51dc2f88e2891d0ccf00655a76a180
Ben Firshman <ben@firshman.co.uk>
2023-01-31 06:34:51 -0800

    Update description

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ import { getRandomSeed } from "lib/seeds";
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 export const appName = "Paint by Text";
-export const appSubtitle = "Modify images by chatting with a generative AI model.";
-export const appMetaDescription = "A web app that lets you modify images by chatting with a generative AI model.";
+export const appSubtitle = "Edit your photos using written instructions, with the help of an AI.";
+export const appMetaDescription = "Edit your photos using written instructions, with the help of an AI.";
 
 export default function Home() {
   const [events, setEvents] = useState([]);

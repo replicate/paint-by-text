f1b105d58fd26247342e6506ef1ad1f367516e0a
Zeke Sikelianos <zeke@sikelianos.com>
2023-02-01 12:56:26 -0800

    check for Replicate API token in env

--- pages/api/predictions/index.js
+++ pages/api/predictions/index.js
@@ -1,6 +1,10 @@
 const API_HOST = process.env.REPLICATE_API_HOST || "https://api.replicate.com";
 
 export default async function handler(req, res) {
+  if (!process.env.REPLICATE_API_TOKEN) {
+    throw new Error("The REPLICATE_API_TOKEN environment variable is not set. See README.md for instructions on how to set it.");
+  }
+  
   // remnove null and undefined values
   req.body = Object.entries(req.body).reduce(
     (a, [k, v]) => (v == null ? a : ((a[k] = v), a)),

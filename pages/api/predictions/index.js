92504ffdc58403db2a270406dfb70f1bdbdb2ba2
Zeke Sikelianos <zeke@sikelianos.com>
2025-05-29 12:45:00 -0700

    use the official model and the latest replicate client

--- pages/api/predictions/index.js
+++ pages/api/predictions/index.js
@@ -23,23 +23,13 @@ export default async function handler(req, res) {
   );
 
   let prediction
-  if (process.env.USE_REPLICATE_DEPLOYMENT) {
-    console.log("Using deployment")
-    prediction = await replicate.deployments.predictions.create(
-      "replicate",
-      "paint-by-text",
-      {
-        input: req.body
-      }
-    );
-  } else {
-    console.log("Not using deployment")
-    const version = "d8ac233ef32825fd74937c891a2d56536d2736bcbd44b54d37ca5c2652a0a051"
-    prediction = await replicate.predictions.create({
-      version, 
-      input: req.body
-    });
-  }
+
+  const model = "black-forest-labs/flux-kontext-pro"
+  prediction = await replicate.predictions.create({
+    model,
+    input: req.body
+  });
+  
 
   console.log({prediction});
 
